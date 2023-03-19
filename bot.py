b0053c08747a47f28ad609120ed906a678bce296
= <=>
2023-03-19 16:18:13 +1300

    Starting files

---
+++ bot.py
@@ -0,0 +1,18 @@
+import os
+import discord
+from dotenv import load_dotenv
+load_dotenv()
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+client  = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected.')
+
+@client.event
+async def on_message(message): 
+    print(message.author)
+
+client.run(DISCORD_TOKEN);
+
